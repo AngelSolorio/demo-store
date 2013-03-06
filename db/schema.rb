0db1dbaeb2f81d5838d8a9ebcb0807c8662fd309
Angel Solorio <angel.solorio@crowdint.com>
2013-03-06 16:13:44 -0600

    Stable snapshot with some tests to the models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130306190015) do
+ActiveRecord::Schema.define(version: 20130306220413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,13 @@ ActiveRecord::Schema.define(version: 20130306190015) do
   end
 
   create_table "images", force: true do |t|
+    t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "products", force: true do |t|
@@ -37,10 +42,6 @@ ActiveRecord::Schema.define(version: 20130306190015) do
     t.text     "tags"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
     t.integer  "admin_id"
   end
 
