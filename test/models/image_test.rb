2482d0fa12facdf7300816a042eb7c43ed96618e
Angel Solorio <angel.solorio@crowdint.com>
2013-03-06 17:47:02 -0600

    more tests to image and product models

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,11 +1,18 @@
 require "minitest_helper"
 
 describe Image do
-  before do
-    @image = Image.new
+  it "must be valid with correct attributes" do
+    admin = Admin.create email: 'admin@store.com', password: '123pass', password_confirmation: '123pass'
+    product = Product.create name: 'TV', admin_id: admin.id, description: 'Samsung Smart TV', price: 120000.00, inventory: 2, active: true
+    image = Image.create product_id: product.id
+    
+    image.valid?.must_equal true
   end
-
-  it "must be valid" do
-    @image.valid?.must_equal true
+  
+  it "must be invalid without attributes" do
+    image = Image.new
+    
+    image.valid?.must_equal false
+    image.errors[:product_id].wont_be_empty
   end
 end
