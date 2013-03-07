0349a4654d1f3e257e01d7922522c1d99e94e77c
memoxmrodlr <memoxmrdl@memoxmrdl-pc.(none)>
2013-03-07 17:35:00 -0600

    working products

--- app/controllers/backend/products_controller.rb
+++ app/controllers/backend/products_controller.rb
@@ -1,37 +1,53 @@
 class Backend::ProductsController < ApplicationController
 	def index
-		@products = current_admin.products
+		@products = Product.my_products(current_admin)
 	end
 
 	def new
 		@product = Product.new
 		5.times { @product.images.build }
-
-		respond_to do |format|
-      format.html
-    end		
 	end
 
 	def create
 	  @product = Product.new(product_params)
+	  @product.inventory = -1 if params[:product][:inventory_check] == "1" 
 	  @product.admin = current_admin
+	  upload_files params[:product][:images_attributes]
 
-	  unless params[:product][:images_attributes].nil?
-	  		params[:product][:images_attributes].each do |i, v|
-	  			@product.images.build v
-	  		end
+	  if @product.save
+	  	return redirect_to backend_products_path, notice: "Producto creado"
 	  end
 
-	  if @product.valid?
-	  	@product.save
-	  	return redirect_to backend_products_path
-	  end
+	  flash[:notice] = "Product no creado"
+	  5.times { @product.images.build }
+	  render :new 
+	end
 
-	  respond_with @product
+	def edit
+   	@product = Product.my_product(params[:id], current_admin).first
+    
+    if @product.nil?
+      return redirect_to backend_products_path, alert: "Producto no encontrado"
+    end
+    
+    (5 - @product.images.count).times { @product.images.build }
+    
+    if @product.inventory == -1
+      @product.inventory = ""
+      @product.inventory_check = true 
+    end
 	end
 
 	protected
   def product_params 
   	params.require(:product).permit(:name, :description, :price, :inventory, :active, :tags)
+  end
+
+  def upload_files files
+  	unless files.blank?
+	  		files.each do |key, value|
+	  			@product.images.build value
+	  		end
+	  end
   end
 end
