e95d6baa4247d9cb80f6ad1143ce58abd46ed9a7
memoxmrodlr <memoxmrdl@memoxmrdl-pc.(none)>
2013-03-07 14:22:34 -0600

    add more options scopes, messages to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,20 @@
 class Product < ActiveRecord::Base
+  attr_accessor :inventory_check
+  
   belongs_to :admin
   has_many :images
 
-  validates :name, :uniqueness => { :scope => :admin_id, :message => "Nombre de producto repetido", :case_sensitive => false }
   validates :name, :admin_id, :description, :price, :inventory, presence: true
-<<<<<<< HEAD
-  validates :price, :numericality => { :greater_than => 0 }
-  validates :inventory, :numericality => { :only_integer => true, :greater_than => -1 }
+
+  validates :name, :uniqueness => { :scope => :admin_id, :message => "Nombre de producto repetido", :case_sensitive => false }
+  
+  validates :price, :numericality => { :greater_than => 1, :message => "Precio incorrecto" }
 
   accepts_nested_attributes_for :images
-=======
+
   validates :price, :numericality => { :greater_than => 0, :message => "Debe ser una cantidad mayor a 0" }
-  validates :inventory, :numericality => { :only_integer => true, :greater_than => -1, :message => "No se permiten cantidades negativas" }
->>>>>>> 41009aac9576980d9a1205f27bf43ec5e952ddfd
+  validates :inventory => { :only_integer => true, :greater_than => -1, :message => "No se permiten cantidades negativas" }
+
+  scope :my_products, -> (admin) { where(:admin_id => admin.id).order(:id) }
+  scope :my_product, -> (product_id, admin) { where(:id => product_id, :admin_id => admin.id) }
 end
