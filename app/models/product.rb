33fc28bf9d5cc7b968548df1bb4ecddf27a241f9
memoxmrodlr <memoxmrdl@memoxmrdl-pc.(none)>
2013-03-07 16:27:33 -0600

    Again up file modified

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,15 +5,11 @@ class Product < ActiveRecord::Base
   has_many :images
 
   validates :name, :admin_id, :description, :price, :inventory, presence: true
-
-  validates :name, :uniqueness => { :scope => :admin_id, :message => "Nombre de producto repetido", :case_sensitive => false }
-  
+  validates :name, :uniqueness => { :scope => :admin_id, :message => "Nombre de producto repetido", :case_sensitive => false }  
   validates :price, :numericality => { :greater_than => 1, :message => "Precio incorrecto" }
+  validate :inventory => { :only_integer => true, :greater_than_or_equal_to => -1, :message => "No se permiten cantidades negativas" }
 
   accepts_nested_attributes_for :images
-
-  validates :price, :numericality => { :greater_than => 0, :message => "Debe ser una cantidad mayor a 0" }
-  validates :inventory => { :only_integer => true, :greater_than => -1, :message => "No se permiten cantidades negativas" }
 
   scope :my_products, -> (admin) { where(:admin_id => admin.id).order(:id) }
   scope :my_product, -> (product_id, admin) { where(:id => product_id, :admin_id => admin.id) }
