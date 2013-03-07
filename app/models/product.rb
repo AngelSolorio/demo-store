41009aac9576980d9a1205f27bf43ec5e952ddfd
Angel Solorio <angel.solorio@crowdint.com>
2013-03-07 10:38:26 -0600

    Update product.rb
    Validates added to the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,6 @@ class Product < ActiveRecord::Base
 
   validates :name, :uniqueness => { :scope => :admin_id, :message => "Nombre de producto repetido", :case_sensitive => false }
   validates :name, :admin_id, :description, :price, :inventory, presence: true
-  validates :price, :numericality => { :greater_than => 0 }
-  validates :inventory, :numericality => { :only_integer => true, :greater_than => -1 }
-
+  validates :price, :numericality => { :greater_than => 0, :message => "Debe ser una cantidad mayor a 0" }
+  validates :inventory, :numericality => { :only_integer => true, :greater_than => -1, :message => "No se permiten cantidades negativas" }
 end
