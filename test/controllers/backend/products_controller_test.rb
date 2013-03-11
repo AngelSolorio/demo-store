dea9d29088820ea84d2cd6ec24e0a741ae2db008
Angel Solorio <angel.solorio@crowdint.com>
2013-03-11 12:34:39 -0500

    some tests added to the product controller

--- test/controllers/backend/products_controller_test.rb
+++ test/controllers/backend/products_controller_test.rb
@@ -1,7 +1,126 @@
 require "minitest_helper"
 
 describe Backend::ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    stub_current_admin
+  end
+  
+  describe 'show' do
+    it "when admin have no products" do
+      stub_current_admin 200
+
+      #get :show
+
+      assert_redirected_to new_backend_product_path
+    end
+
+    it "when admin have products" do
+      @product = Product.my_product(1, (stub_current_admin 100)).first
+      
+      #get :show
+
+      assert_not_nil(@product, '')
+      assert_response :success
+      assert_template :show
+      assigns[:product].wont_be_nil
+    end
+  end
+  
+  describe 'new' do
+    let(:params) {
+      {
+        product: { name: 'Toshiba TV', description: 'Toshiba 3D Smart TV 50"', price: 1200, inventory: 2, active: true },
+        admin: { id: 1 } 
+      }
+    }
+    
+    it "should method new display a form to register a new product" do
+      [:params][:admin][:id] = 2
+      
+      get :new, params
+
+      assert_response :success
+      assert_template :new
+      assigns[:product].wont_be_nil
+    end
+
+    it "should method new display a form to register a new product even if admin already has products" do
+      get :new, params
+
+      assert_response :success
+      assert_template :new
+      assigns[:product].wont_be_nil
+    end
+  end
+  
+  describe 'edit' do
+    let(:params) {
+      {
+        product: { id: 1, name: 'Samsung TV', description: 'Samsung Smart TV 46"', price: 8500, inventory: 4, active: true } 
+      }
+    }
+    
+    it "should method edit display a form to edit the product attributes if admin own the product" do
+      @product = Product.my_product(params[:product][:id], (stub_current_admin 100)).first
+      
+      #get :edit, { 'product_id' => params[:id]}
+      
+      assert_not_nil(@product, '')
+      assert_response :success
+      assert_template :edit
+      assigns[:product].wont_be_nil
+    end
+
+    it "should redirect to new when user have no products to edit" do
+      @product = Product.my_product(params[:product][:id], (stub_current_admin 200)).first
+
+      #get :edit
+      
+      assert_nil(@product, '')
+      assert_redirected_to new_backend_product_path
+      flash[:notice].wont_be_nil
+    end
+  end
+  
+  describe 'update' do
+    let(:params) {
+      {
+        product: { id: 2, name: 'Samsung TV', description: 'Samsung Smart TV 46"', price: 8500, inventory: 4, active: true }
+      }
+    }
+  
+    it 'should redirect to create product when the admin is not logged in' do
+      stub_current_admin 200
+
+      #put :update, params
+
+      assert_redirected_to new_backend_product_path
+      flash[:notice].wont_be_nil
+    end
+
+    it "should update a product and redirect to product show" do
+      #put :update, params
+
+      assert_redirected_to backend_product_path
+      flash[:notice].wont_be_nil
+    end
+
+    it "should display edit form if product is invalid" do
+      params[:product][:name] = nil
+
+      #put :update, params
+
+      assert_response :success
+      assert_template :edit
+      flash[:alert].wont_be_nil
+    end
+  end
+end
+
+
+def stub_current_admin(id = 100)
+  Backend::ProductController.class_exec(id) do |id|
+    body = -> { @admin ||= Admin.find id }
+    define_method :current_admin, body
+  end
 end
